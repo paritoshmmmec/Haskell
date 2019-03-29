ba02f0d7bfa87df33883afeb95c1fdd6003d5f4d
paritosh baghel <paritoshmmmec@gmail.com>
2019-03-29 18:54:30 -0400

    List comprehension

--- flatMap.hs
+++ flatMap.hs
@@ -1,1 +1,5 @@
+-- Flat Maps using function
 flatMap items = if null items then [] else (head items) * 2 : flatMap(tail items)
+
+-- Flat Maps using list comprehension
+flatMap items = [items* 2  | items<-items ]
